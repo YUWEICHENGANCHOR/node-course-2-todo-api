e02b0062cd5f4fa30c0c5316c70a03fde5ac176a
anchor <anchor@dfischer-1.u15.stevens-tech.edu>
2017-12-12 17:50:13 -0500

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,10 +8,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
     console.log('Connect to MongoDB server');
 
     //deleteMany
+    /*db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result)=>{
+        console.log(result);
+    });*/
 
     //deleteOne
+    /*db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result)=>{
+        console.log(result);
+    });*/
 
     //findOneAndDelete
+    /*db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+        console.log(result);
+    });*/
+
+    /*db.collection('Users').deleteMany({name: 'anchor'}).then((result)=>{
+        console.log(result);
+    });*/
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5a304b46767dd80346984f53")
+    }).then((result) => {
+        console.log(JSON.stringify(result,undefined,2));
+    });
 
 
     //db.close();
